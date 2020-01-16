3b247b7be764aad802299d60922718ec7d4f62bd
jjensen654 <jjensen54@huskers.unl.edu>
2020-01-16 08:23:29 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+//J.P. Jensen, Kenji Tamane
+//1/16/2020
+//Kenji Tamane
+//To print my name, basic hello.c file for GitHub
+
+#include <stdio.h>
+int main(void) {
+	printf("J.P. Jensen\n");
+	return 0;
+}
